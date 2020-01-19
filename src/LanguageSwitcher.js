fc7bef115334637658444c546a90daefd1c37119
Lasse Liehu <lasse.liehu@iki.fi>
2020-01-19 21:06:59 +0200

    Add automatic language switch on user agent language change

--- src/LanguageSwitcher.js
+++ src/LanguageSwitcher.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
+import { withTranslation } from 'react-i18next';
 
-const LanguageSwitcher = (props) => {
-  const { t, i18n } = useTranslation();
-
-  function switchLanguageTo(language) {
-    i18n.changeLanguage(language);
+class LanguageSwitcher extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      manuallySelectedLanguage: false
+    };
+    window.addEventListener('languagechange', () => {
+      if (!this.state.manuallySelectedLanguage) {
+        i18next.changeLanguage(navigator.language)
+      }
+    });
   }
 
-  return (
-    <div>
-      <h2>{t('languageSwitcherTitle')}</h2>
-      <button onClick={switchLanguageTo.bind(null, 'fi')}>Suomi</button>
-      <button onClick={switchLanguageTo.bind(null, 'en')}>English</button>
-    </div>
-  );
+  switchLanguageTo(language) {
+    i18next.changeLanguage(language);
+    this.setState({
+      manuallySelectedLanguage: true
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.props.t('languageSwitcherTitle')}</h2>
+        <button onClick={this.switchLanguageTo.bind(this, 'fi')}>Suomi</button>
+        <button onClick={this.switchLanguageTo.bind(this, 'en')}>English</button>
+      </div>
+    );
+  }
 };
 
-export default LanguageSwitcher;
+export default withTranslation()(LanguageSwitcher);
