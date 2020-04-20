e89f278edb4fc8c63ec26d9d6beb31f700e6a52b
Lasse Liehu <lasse.liehu@iki.fi>
2020-04-20 18:48:48 +0300

    Add messages object to global window object for easier testing

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import messages from './messages';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+window.messages = messages;
+
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
